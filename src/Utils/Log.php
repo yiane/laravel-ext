e7afcd4909d6ac15164f221890cf450ff7937d16
yiane <yiane@qq.com>
2019-09-27 00:50:11 +0800

    更新

--- src/Utils/Log.php
+++ src/Utils/Log.php
@@ -7,108 +7,209 @@
  */
 
 namespace Yr\Utils;
+
+use Exception;
 use Monolog\Formatter\LineFormatter;
 use Monolog\Handler\RotatingFileHandler;
 use Monolog\Logger as MonologLogger;
+use Monolog\Handler\StreamHandler;
+use Psr\Log\LoggerInterface;
+
+/**
+ * 日志记录，调用方式 Log::order($info), 记录日志内容则会生成order-Y-m-d.log日志文件
+ *                  Log::order($info,"warning"), 该记录文件行开头为order.WARNING,支持以下日志级别
+ *                  Log::order($info,[],"warning"), 第二个参数记录日志详情信息数组
+ * 八种日志级别：emergency, alert, critical, error,warning, notice, info 和 debug
+ * 兼容\Illuminate\Support\Facades\Log使用方式，支持自定义文件记录日志
+ *
+ * @method static void emergency($message, array $context = array())
+ * @method static void alert($message, array $context = array())
+ * @method static void critical($message, array $context = array())
+ * @method static void error($message, array $context = array())
+ * @method static void warning($message, array $context = array())
+ * @method static void notice($message, array $context = array())
+ * @method static void info($message, array $context = array())
+ * @method static void debug($message, array $context = array())
+ *
+ */
 class Log
 {
 
-    private static $loggers = [];
-
     /**
-     * The Log levels.
-     * MonologLogger 支持的日志等级
-     * @var array
+     * Logger instance.
+     *
+     * @var Array LoggerInterface
      */
-    protected static $levels = [
-        'debug'     => MonologLogger::DEBUG,
-        'info'      => MonologLogger::INFO,
-        'notice'    => MonologLogger::NOTICE,
-        'warning'   => MonologLogger::WARNING,
-        'error'     => MonologLogger::ERROR,
-        'critical'  => MonologLogger::CRITICAL,
-        'alert'     => MonologLogger::ALERT,
-        'emergency' => MonologLogger::EMERGENCY,
-    ];
+    protected static $loggers = [];
 
     /**
-     * 获取一个实例
-     * @param $type
-     * @param string $name The logging channel
-     * @param int $maxFiles The maximal amount of files to keep (0 means unlimited)
+     * @return array
+     */
+    public static function getLevels()
+    {
+        return MonologLogger::getLevels();
+    }
+
+    /**
+     * @param $level_name
+     * @return bool
+     */
+    public static function checkLevel($level_name)
+    {
+        $levels = MonologLogger::getLevels();
+        return isset($levels[strtoupper($level_name)]);
+    }
+
+    /**
+     * Return the logger instance.
+     * @param string $file_name
+     * @param string $type
+     * @return MonologLogger
+     * @throws Exception
+     */
+    public static function getLogger($file_name = 'laravel', $type = 'daily')
+    {
+        return static::$loggers[$file_name] ?? static::createLogger($file_name, MonologLogger::DEBUG, $type);
+    }
+
+
+    /**
+     * Set logger.
+     * @param LoggerInterface $logger
+     * @param string $file_name
+     */
+    public static function setLogger(LoggerInterface $logger, $file_name = 'laravel')
+    {
+        static::$loggers[$file_name] = $logger;
+    }
+
+    /**
+     * Tests if logger exists.
+     * @param string $file_name
+     * @return bool
+     */
+    public static function hasLogger($file_name = 'laravel')
+    {
+        return isset(static::$loggers[$file_name]) ? true : false;
+    }
+
+    /**
+     * Make a log instance.
+     * @param null $file_name
+     * @param int $level
+     * @param string $type
+     * @param int $max_files
+     * @return MonologLogger
+     * @throws Exception
+     */
+    public static function createLogger($file_name = null, $level = MonologLogger::DEBUG, $type = 'daily', $max_files = 30)
+    {
+        $file_name = $file_name ?? 'laravel';
+        $file = storage_path() . '/logs/' . $file_name . '.log';
+
+        if (empty(static::$loggers[$file_name])) {
+            $handler = $type === 'single' ? new StreamHandler($file, $level) : new RotatingFileHandler($file, $max_files, $level);
+
+            $handler->setFormatter(
+                new LineFormatter(null, null, false, true)
+            );
+            $logger = new MonologLogger($file_name);
+            $logger->pushHandler($handler);
+            static::$loggers[$file_name] = $logger;
+        } else {
+            $logger = static::$loggers[$file_name];
+        }
+
+        return $logger;
+    }
+
+
+    /**
+     * Forward call.
+     * @param $method
+     * @param $args
      * @return mixed
      */
-    public static function getLogger($type, $name = "local", $maxFiles = 0)
+    public static function __callStatic($method, $args)
     {
-        if (empty(self::$loggers[$type])) {
-            // @todo: 暂时根据文件存储，后续升级
-            $handle = new RotatingFileHandler(
-                storage_path().'/logs/'. $type .'.log',
-                $maxFiles
-            );
-            $handle->setFormatter(new LineFormatter());
-            self::$loggers[$type] = new MonologLogger($name,[$handle]);
+        try {
+            if (static::checkLevel($method)) {
+                static::adapterArgs($method, $args);
+                return forward_static_call_array([static::getLogger(), $method], $args);
+            } else {
+                $act = "info";//默认以info形式记录
+                static::adapter($act, $args);
+                return forward_static_call_array([static::getLogger($method), $act], $args);
+            }
+        } catch (Exception $e) {
+
         }
 
-        $log = self::$loggers[$type];
-        return $log;
     }
 
     /**
-     *
-     * 日志记录，调用方式 \App\Log::order($info), 记录日志内容则会生成order-Y-m-d.log日志文件
-     *                  \App\Log::order($info,"warning"), 该记录文件行开头为order.WARNING,支持以下日志级别
-     *                  \App\Log::order($info,[],"warning"), 第二个参数记录日志详情信息数组
-     * 八种日志级别：emergency, alert, critical, error,warning, notice, info 和 debug
-     * 兼容\Illuminate\Support\Facades\Log使用方式，支持自定义文件记录日志
-     * @param string $name
-     * @param $arguments
+     * Forward call.
+     * @param $method
+     * @param $args
+     * @return mixed
      */
-    public static function __callStatic(string $name, $arguments){
-        if(is_array($arguments) && !empty($arguments)){
-            $type = $name;
-            $act = "info";//默认以info形式记录
-            $context = [];
-            if(isset(static::$levels[$name])){//兼容框架调用,记录日志到laravel文件
-                $type = "laravel";
-                $act = strtolower($name);
-                (isset($arguments[1]) && is_array($arguments[1]) ) and $context = $arguments[1];
-            }else{
-                if(count($arguments) > 1){
-                    $end = array_pop($arguments);
-                    if(is_array($end)){
-                        $context = $end;
-                    }else{
-                        $end = strtolower($end);
-                        if(isset(static::$levels[$end])){
-                            $act = strtolower($end);
-                        }
-                        if(count($arguments) > 1 && is_array($arguments[1])){
-                            $context = $arguments[1];
-                        }
-                    }
-                }
-            }
-            try{
-                if(env("APP_LOG_LEVEL") == "debug" && $act == "error" && empty($context)){//测试日志记录堆栈信息
-                    $arr = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 10);
-                    foreach ($arr as $key => $row){
-                        !isset($row["type"]) and $row["type"] = " ";
-                        if(isset($row["file"])){
-                            $context[] = "#$key " . $row["file"] . "(".$row["line"].")".$row["type"].$row["function"];
-                        }else{
-                            $context[] = "#$key " . $row["class"] . $row["type"] . $row["function"];
-                        }
+    public function __call($method, $args)
+    {
+        try {
+            if (static::checkLevel($method)) {
+                static::adapterArgs($method, $args);
 
-                    }
-                }
-                static::getLogger($type)->{$act}($arguments[0],$context);
-            }catch (\Exception $e){
+                return call_user_func_array([static::getLogger(), $method], $args);
+            } else {
+                $act = "info";//默认以info形式记录
+                static::adapter($act, $args);
 
+                return call_user_func_array([static::getLogger($method), $act], $args);
             }
+        } catch (Exception $e) {
 
         }
     }
 
+    /**
+     * @param $act
+     * @param $args
+     */
+    public static function adapter(&$act, &$args)
+    {
+        if (count($args) > 1) {
+            $end = array_pop($args);
+            if (is_array($end)) {
+                $args = [
+                    $args[0],
+                    $end
+                ];
+            } elseif (static::checkLevel($end)) {
+                $act = $end;
+            }
 
+        }
+
+        static::adapterArgs($act, $args);
+    }
+
+    /**
+     * @param $act
+     * @param $args
+     */
+    public static function adapterArgs($act, &$args)
+    {
+        if (strtoupper($act) == 'ERROR' && (!isset($args[1]) || empty($args[1]))) {
+            $arr = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 5);
+            foreach ($arr as $key => $row) {
+                !isset($row["type"]) and $row["type"] = " ";
+                if (isset($row["file"])) {
+                    $args[1][] = "#$key " . $row["file"] . "(" . $row["line"] . ")" . $row["type"] . $row["function"];
+                } else {
+                    $args[1][] = "#$key " . $row["class"] . $row["type"] . $row["function"];
+                }
+            }
+        }
+
+    }
 }
